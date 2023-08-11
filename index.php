55bc456a18112aaebaf8eb88b8a225db64495e81
Stefano Degiorgi <stef.degiorgi@bluewin.ch>
2023-08-11 12:29:30 +0200

    navigation bar

--- index.php
+++ index.php
@@ -1,17 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My Website</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="icon" href="./favicon.ico" type="image/x-icon">
-  </head>
-  <body>
-    <main>
-        <h1>Welcome to My Website</h1>  
-    </main>
-	<script src="index.js"></script>
-  </body>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Protfolio | Stefano Degiorgi</title>
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="icon" href="./favicon.ico" type="image/x-icon">
+  <script src="index.js"></script>
+</head>
+
+<body>
+  <div class="nav-container">
+    <header>
+      <main>Stefano Degiorgi</main>
+      <nav>
+        <ul>
+          <li><a href="#about">About me</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#contact">Contact me</a></li>
+        </ul>
+      </nav>
+    </header>
+    <div class="social-header">
+      <ul>
+      </ul>
+    </div>
+  </div>
+  
+</body>
 </html>
